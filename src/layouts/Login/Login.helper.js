fd68be410432df7ca1c9a890ffcb9a2831aeda50
Nitheesh <nitheeshkr115@gmail.com>
2019-10-03 21:13:31 +0530

    Initial commit

---
+++ src/layouts/Login/Login.helper.js
@@ -0,0 +1,9 @@
+import { httpGet } from '@communication/communication';
+
+const loginHelper = {
+  login: () => {
+    httpGet();
+  },
+};
+
+export default loginHelper;
---
+++ src/layouts/Login/Login.js
@@ -0,0 +1,32 @@
+import BusinessTest from '@business/BusinessTest/BusinessTest.vue';
+import colorChanger from '@directives/colorChanger';
+import currency from '@filters/currency';
+import data from '@mocks/test.json';
+import loginHelper from './Login.helper';
+
+export default {
+  name: 'Dashboard',
+  directives: { colorChanger },
+  filters: { currency },
+  data() {
+    return {
+      id: '',
+      pageName: 'Dashboard',
+      styleData: 'color',
+      dummyData: data,
+      password: '',
+      username: '',
+    };
+  },
+  methods: {
+    loginUser() {
+      loginHelper.login().then(res => {
+        console.log(res);
+      });
+    },
+  },
+  watch: {},
+  components: {
+    BusinessTest,
+  },
+};
